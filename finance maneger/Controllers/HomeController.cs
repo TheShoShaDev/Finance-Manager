144026957422fc621ba13763035a8d4e9879f2f9
TheShoShaDev <ratattyu007@mail.ru>
2023-11-23 21:08:36 +0700

    Добавьте файлы проекта.

---
+++ finance maneger/Controllers/HomeController.cs
@@ -0,0 +1,51 @@
+﻿using finance_maneger.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace finance_maneger.Controllers
+{
+	public class HomeController : Controller
+	{
+		private readonly ILogger<HomeController> _logger;
+
+		public HomeController(ILogger<HomeController> logger)
+		{
+			_logger = logger;
+		}
+
+		[Authorize]
+		public IActionResult Index()
+		{
+			return View();
+		}
+
+		public IActionResult Privacy()
+		{
+			return View();
+		}
+
+		[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+		public IActionResult Error()
+		{
+			return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+		}
+
+		public IActionResult Detele(int id)
+		{
+			throw new Exception("It's not done yet!");
+			return View();
+		}
+
+		public IActionResult Edit(int id)
+		{
+			throw new Exception("It's not done yet!");
+			return View();
+		}
+
+		public IActionResult AddTransaction()
+		{
+			return View();
+		}
+	}
+}
